222acdcde4e271497250a009c418c9d312386f7e
Toffee1347 <danielnhoward93@gmail.com>
2022-12-03 12:58:30 +0000

    Update 3

--- challenges/year2022/day03.go
+++ challenges/year2022/day03.go
@@ -4,4 +4,10 @@ import "strings"
 
 func Day03(input string) [2]int {
 	rucksacks := strings.Split(input, "\r\r\n")
+
+	for _, rucksack := range rucksacks {
+		size := len(rucksack)
+		firstComp := rucksack[0:size/2]
+		secondComp := rucksack[size/2:size]
+	}
 }
